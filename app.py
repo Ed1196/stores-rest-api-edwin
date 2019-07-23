3904482ef3136fce02e660cfa4c1e563082982bd
Ed1196 <34897031+Ed1196@users.noreply.github.com>
2019-07-23 12:04:59 -0400

    Added run file

--- app.py
+++ app.py
@@ -17,12 +17,6 @@ app = Flask(__name__)
 #Api: Allow us to add/remove/update resources, all have to be a class
 api = Api(app)
 
-# With this flask decorator will run the method below it before the first request
-# db.create_all() will create all the tables in the database
-@app.before_first_request
-def create_tables():
-    db.create_all()
-
 #Key that will be use for decryption
 app.secret_key = 'Edwin'
 
