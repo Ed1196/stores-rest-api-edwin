b70f369707fbd3b46305c936e503d2b06ae45fdf
Ed1196 <34897031+Ed1196@users.noreply.github.com>
2019-08-06 15:05:01 -0400

    removed jwt_extended

--- resources/user.py
+++ resources/user.py
@@ -1,25 +1,24 @@
 import sqlite3
 from flask_restful import Resource, reqparse
 from models.user import UserModel
-from werkzeug.security import safe_str_cmp
-
-#Variable that will allow us to parse the data
-_user_parser = reqparse.RequestParser()
-#Here is where we specify the fields that we want from the payload
-_user_parser.add_argument('username',
-        type=str,
-        required=True,
-        help='This field cannot be left blank!')
-_user_parser.add_argument('password',
-        type=str,
-        required=True,
-        help='This field cannot be left blank!')
 
 class UserRegister(Resource):
 
+    #Variable that will allow us to parse the data
+    parser = reqparse.RequestParser()
+    #Here is where we specify the fields that we want from the payload
+    parser.add_argument('username',
+        type=str,
+        required=True,
+        help='This field cannot be left blank!')
+    parser.add_argument('password',
+        type=str,
+        required=True,
+        help='This field cannot be left blank!')
+
     def post(self):
         #We store the data that we parsed into a variable
-        data =  _user_parser.parse_args()
+        data = UserRegister.parser.parse_args()
 
         #Check if user already exists: Use the findByUserName method from User
         if(UserModel.findByUserName(data['username']) ):
@@ -32,7 +31,6 @@ class UserRegister(Resource):
         return {'message':'User was created succesfully!'}, 201
 
 class User(Resource):
-
     @classmethod
     def get(cls, user_id):
         user = UserModel.findById(user_id) 
@@ -46,53 +44,3 @@ class User(Resource):
             user.delete_from_db()
             return {'message': 'User deleted succesfully.'}
         return {'message': 'User does not exist.'}
-
-class UserLogin(Resource):
-   
-    @classmethod
-    def post(cls):
-        # get data from parser
-        data =  _user_parser.parse_args()
-        # find user in database
-        user = UserModel.findByUserName(data['username'])
-        # check password
-        # create access token
-        # create refresh token
-        if user and safe_str_cmp(user.password, data['password']):
-            #JWT_Ex creates the JWT here that the user will use to make calls to the api
-            access_token = create_access_token(identity=user.id, fresh=True)
-            refresh_token = create_refresh_token(user.id)
-            return {
-                'access_token': access_token,
-                'refresh_token': refresh_token
-            }, 200
-        return {'message': 'Invalid Credentials'}, 401
-        
-class TokenRefresh(Resource):
-    @jwt_refresh_token_required # Deny access if refresh_token is not in the request.
-    def get(self):
-        current_user = get_jwt_identity()
-        # If fresh==True; password and username was given to us.
-        # If fresh==False; password and username were given days ago.
-        new_token = create_access_token(identity=current_user, fresh=False)
-        return {'access_token': new_token}, 200
-
-class UserList(Resource):
-    @jwt_optional
-    def get(self):
-        user_id = get_jwt_identity()
-        if user_id:
-            users = [user.json() for user in UserModel.find_all()]
-            return {
-                'users': [user for user in users]
-            }
-        return {'message': 'Log in to get list of users.'}
-
-class UserLogout(Resource):
-    
-    @jwt_required
-    def post(self):
-        jti = get_raw_jwt()['jti']
-        BLACKLIST.add(jti)
-        return {'message': 'Succesful logout'}, 200
-
